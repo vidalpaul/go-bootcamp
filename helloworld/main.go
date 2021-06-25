0de4e8e2028d14c417e948b38c80895c98a045c4
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-06-25 15:55:48 -0300

    first 2 sections completed

--- helloworld/main.go
+++ helloworld/main.go
@@ -7,8 +7,14 @@ func main() {
 }
 
 // 5 questions
-// 1 how do we run the code in our project?
+// 1 how do we run the code in our project? go run file.go
 // 2 what does package main mean?
+// 2 types of pkg: a executable pkg generates a file that we can run and a reusable pkg which is used as helpers
+// package == project == workspace
+// executable pkgs must have a func main()
 // 3 what does import fmt mean?
+// format standard library
+// golang.org/pkg
 // 4 whats is that func thing?
 // 5 how is the main.go file organized?
+// pkg declaration + imports + declare and run functions
