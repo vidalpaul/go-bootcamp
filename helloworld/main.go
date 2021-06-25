457bf0b09b3e4017b6c90018d36fcae5b2f323b7
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-06-25 15:37:10 -0300

    Initial commit

---
+++ helloworld/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+// 5 questions
+// 1 how do we run the code in our project?
+// 2 what does package main mean?
+// 3 what does import fmt mean?
+// 4 whats is that func thing?
+// 5 how is the main.go file organized?
