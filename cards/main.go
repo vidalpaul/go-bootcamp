952bd86702b23114feeb7bb08ba12f89ab4937b1
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-06-30 17:52:00 -0300

    Update main.go

--- cards/main.go
+++ cards/main.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 )
 
+var deckSize int
+
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
 	fmt.Println(card)
+	fmt.Println(deckSize)
 }
