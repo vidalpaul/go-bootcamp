b9cf05e79141cb14d57bf5f5a0f354770379e51f
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-02 23:15:48 -0300

    new deck function implemented

--- cards/main.go
+++ cards/main.go
@@ -1,16 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{newCard(), "Ace of Diamonds"}
-	cards = append(cards, "Six of Spades")
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	cards := newDeck()
 
-}
+	cards.print()
 
-func newCard() string {
-	return "Five of Diamonds"
 }
